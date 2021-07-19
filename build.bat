dab2575d71e6406140b29aa08fd09739be2775e9
Victor Oliveira <asaptorvi@protonmail.com>
2021-07-19 04:05:11 -0300

    chore: delete build files

--- build.bat
+++
@@ -1,3 +0,0 @@
-dotnet restore
-dotnet build ./src/BackendChallenge --no-restore
-dotnet test ./tests/BackendChallenge.Tests --no-build
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dotnet restore
-dotnet build ./src/BackendChallenge --no-restore
-dotnet test ./tests/BackendChallenge.Tests --no-build
